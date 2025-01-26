ff9d4d38717d9e44818fd2b81fc1511dfcecf117
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-01-26 11:42:40 +0100

    final random walk

--- day-18-start/random_walk.py
+++ day-18-start/random_walk.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-from random import randint
+from random import randint, choice, sample
 
 timmy_the_turtle = Turtle()
 
@@ -9,7 +9,13 @@ def random_color():
     b = randint(0, 255)
     timmy_the_turtle.color(r/255, g/255, b/255)
 
+move_1 = [timmy_the_turtle.forward, timmy_the_turtle.backward]
+move_2 = [timmy_the_turtle.right, timmy_the_turtle.left]
 
+for _ in range(100):
+    random_color()
+    choice(move_1)(30)
+    choice(move_2)(90)
 
 screen = Screen()
 screen.exitonclick()
