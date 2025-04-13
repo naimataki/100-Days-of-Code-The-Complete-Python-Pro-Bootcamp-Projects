e5f7690f375ea90929a8e740fb59f620d158b2c9
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-04-13 20:31:23 +0100

    Hirst Painting

---
+++ Hirst Painting/main.py
@@ -0,0 +1,3 @@
+import colorgram as cg
+
+colors = cg.extract('painting.jpg', 6)
--- day-18-start/spirograph.py
+++ day-18-start/spirograph.py
@@ -13,18 +13,18 @@ def random_color():
     return (r, g, b)
 
 def draw_spirograph(size_of_gap):
-    for _ in range(360 / size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
         timmy_the_turtle.color(random_color())
         timmy_the_turtle.circle(100)
         timmy_the_turtle.setheading(timmy_the_turtle.heading() + size_of_gap)
 
-draw_spirograph(10)
+draw_spirograph(5)
 
 #timmy_the_turtle.circle(200)
-for _ in range(72):
-    timmy_the_turtle.color(random_color())
-    timmy_the_turtle.circle(100)
-    timmy_the_turtle.circle(200, 5)
+#for _ in range(72):
+#    timmy_the_turtle.color(random_color())
+#    timmy_the_turtle.circle(100)
+#    timmy_the_turtle.circle(200, 5)
 
 screen = Screen()
 screen.exitonclick()
