ee0169614acce9d7902b399229af6c0ffb1eff66
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-04-20 10:00:39 +0100

    final Hirst painting

--- Hirst Painting/main.py
+++ Hirst Painting/main.py
@@ -5,11 +5,25 @@ colormode(255)
 color_list = [(190, 172, 0), (208, 0, 103), (0, 148, 60), (253, 69, 0), (217, 229, 234), (167, 0, 0), (0, 128, 205), (89, 0, 95), (254, 223, 0), (232, 234, 232), (42, 192, 234), (0, 0, 0), (248, 19, 152), (253, 7, 4), (235, 11, 143), (253, 4, 6), (15, 190, 234), (236, 153, 69), (250, 51, 17), (238, 215, 82), (16, 167, 127), (224, 126, 168), (234, 163, 190), (114, 193, 156), (160, 210, 177), (142, 209, 228), (239, 170, 157), (106, 120, 184)]
 
 tim = Turtle()
+tim.hideturtle()
+tim.up()
 tim.speed(0)
+initial_x = -250
+initial_y = -250
 
-tim.color(choice(color_list))
-tim.dot(20)
-tim.setpos(50); tim.dot(20); tim.setpos(50)
+for i in range(10):
+    tim.setx(initial_x)
+    tim.sety(initial_y)
+
+    for j in range(10):
+        #tim.color(choice(color_list))
+        tim.dot(20, choice(color_list))
+        tim.forward(50)
+
+    initial_y += 50
+
+#print(tim.pos())
+# tim.dot(20); tim.setpos(50)
 
 
 
