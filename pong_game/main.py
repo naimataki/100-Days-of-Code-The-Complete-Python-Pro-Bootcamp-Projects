3c8a0d273f568769dca0c7de3cdcdb79068259f9
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-06-22 17:08:16 +0100

    Scoreboard start

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,9 +1,46 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("My pong game")
 screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle= Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    #time.sleep(0.1)
+    ball.move()
+
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    
+    #Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        #print("Made contact")
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        ball.bounce_x()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 
 screen.exitonclick()
--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -1,16 +1,27 @@
 from turtle import Turtle
-WIDTH = 20
-HEIGHT = 20
-XPOS = 350
-YPOS = 0
-UP = 90
-DOWN = 270
+#WIDTH = 20
+#HEIGHT = 20
+UP = 0
+DOWN = 180
+MOVE_DISTANCE = 20
 
-class Paddle:
+class Paddle(Turtle):
 
-    def __init__(self):
-        paddle = Turtle("square")
-        paddle.shapesize(WIDTH, HEIGHT)
-        paddle.color("white")
-        paddle.penup()
-        paddle.goto((XPOS, YPOS))
+    def __init__(self, position):
+        super().__init__()
+        #self.hideturtle()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(position)
+        #self.showturtle()
+
+
+    def up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
