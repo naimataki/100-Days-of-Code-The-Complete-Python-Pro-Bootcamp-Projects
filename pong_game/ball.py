80d23315654a155be2faf6862ba88a8d853db2a0
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-06-23 13:30:34 +0100

    ping pong final and turtle crossing start

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -10,6 +10,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -21,6 +22,13 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *=  -1
+        self.move_speed * 0.9
+
+    #def speed(self):
+    #    self.x_move += 5
+    #    self.y_move += 5
 
     def reset_position(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- pong_game/main.py
+++ pong_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle= Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -21,7 +23,7 @@ screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     #time.sleep(0.1)
     ball.move()
@@ -33,14 +35,15 @@ while game_is_on:
     #Detect collision with r_paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         #print("Made contact")
-        ball.bounce_x()
+        #ball.speed()
+        ball.bounce_x() 
 
     if ball.xcor() > 380:
         ball.reset_position()
-        ball.bounce_x()
+        scoreboard.l_point()
 
     if ball.xcor() < -380:
         ball.reset_position()
-
+        scoreboard.r_point()
 
 screen.exitonclick()
