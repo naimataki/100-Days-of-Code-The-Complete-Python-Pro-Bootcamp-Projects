8f0cd5536d62a6848725df77e7d1651872f8f279
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-06-24 15:41:40 +0100

    turtle crossing start

--- turtle_crossing/scoreboard.py
+++ turtle_crossing/scoreboard.py
@@ -1,5 +1,21 @@
 FONT = ("Courier", 24, "normal")
 
+from turtle import Turtle
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        #self.color("white") 
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-200, 240)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.update_scoreboard()
