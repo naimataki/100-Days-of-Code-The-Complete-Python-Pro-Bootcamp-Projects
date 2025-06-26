d50752efdc33ebb404036b18e071200fc23e9762
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-06-26 16:19:13 +0100

    nothing

--- turtle_crossing/car_manager.py
+++ turtle_crossing/car_manager.py
@@ -3,7 +3,7 @@ from random import choice, randint
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+#naimataki
 
 class CarManager(Turtle):
 
