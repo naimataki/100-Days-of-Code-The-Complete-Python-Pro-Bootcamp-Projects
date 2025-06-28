337ab2ff45e23de8fcd79485b13d3be3c11a172b
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-06-28 12:29:13 +0100

    turtle crossing final

--- turtle_crossing/car_manager.py
+++ turtle_crossing/car_manager.py
@@ -5,17 +5,26 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 #naimataki
 
-class CarManager(Turtle):
+class CarManager():
 
     def __init__(self):
-        super().__init__()
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
-        self.shape("square")
-        self.shapesize(stretch_wid=2, stretch_len=1)
-        self.color(choice(COLORS))
-        self.penup()
-        self.setheading(90)
-        self.setx(320) 
-        self.sety(randint(-250, 250)) 
-        self.move_speed = 0.1
-        self.forward(STARTING_MOVE_DISTANCE)
+    def create_car(self):
+        random_chance = randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(choice(COLORS))
+            new_car.penup()
+            random_y = randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def increase_speed(self):
+        self.car_speed += MOVE_INCREMENT
--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -11,21 +11,30 @@ screen.tracer(0)
 
 player = Player()
 scoreboard = Scoreboard()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.up, "Up")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(car_manager.move_speed)
+    time.sleep(0.1)
     screen.update()
 
-    car_manager = CarManager()
-    #car_manager.move()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
     if player.ycor() == 280:
         player.goto(0, -280)
         scoreboard.level_up()
+        car_manager.increase_speed()
+
+
 
 
 
