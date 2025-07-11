0a0ba060943b711b63c80524d994cf404c4feb4e
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-07-11 17:57:54 +0100

    us states game start

---
+++ US states game/main.py
@@ -0,0 +1,8 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "US states game/blank_states_img.gif"
+screen.addshape()
+
+screen.exitonclick()
--- day-25/main.py
+++ day-25/main.py
@@ -14,7 +14,7 @@
 
 import pandas
 
-data = pandas.read_csv("day-25/weather_data.csv")
+#data = pandas.read_csv("day-25/weather_data.csv")
 #print(type(data))
 #print(data['temp'])
 #print(data)
@@ -39,16 +39,32 @@ data = pandas.read_csv("day-25/weather_data.csv")
 #max_temp = data.temp.max()
 #print(data[data.temp == max_temp])
 
-monday = data[data.day == "Monday"]
+#monday = data[data.day == "Monday"]
 #print(monday.condition)
 
-monday_temp = monday.temp[0]
-monday_temp_F = monday_temp * (9 / 5) + 32
-print(monday_temp_F)
+#monday_temp = monday.temp[0]
+#monday_temp_F = monday_temp * (9 / 5) + 32
+#print(monday_temp_F)
 
 #Create a dataframe from scratch
 
+#data_dict = {
+#    "students": ["Naima", "Reda", "Najiba"],
+#    "scores": [76, 56, 65]
+#}
+#data = pandas.DataFrame(data_dict)
+##print(data)
+#data.to_csv("day-25/new_data.csv")
+
+data = pandas.read_csv("day-25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray = len(data[data["Primary Fur Color"] == "Gray"])
+black = len(data[data["Primary Fur Color"] == "Black"])
+cinnamon = len(data[data["Primary Fur Color"] == "Cinnamon"])
+
 data_dict = {
-    "students": ["Naima", "Reda", "Najiba"],
-    "scores": [76, 56, 65]
+    "Fur color": ["gray", "black", "cinnamon"],
+    "Count": [gray, black, cinnamon]
 }
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("day-25/squirrel_color_count.csv")
