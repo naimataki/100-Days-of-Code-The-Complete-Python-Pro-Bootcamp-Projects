041966fc182b97892b795e8cea32967bac31acbb
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-07-12 19:10:22 +0100

    us states game 2

--- US states game/main.py
+++ US states game/main.py
@@ -3,6 +3,8 @@ import turtle
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "US states game/blank_states_img.gif"
-screen.addshape()
+screen.addshape(image)
+
+turtle.shape(image)
 
 screen.exitonclick()
