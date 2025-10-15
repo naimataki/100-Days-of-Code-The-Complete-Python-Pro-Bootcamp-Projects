6109be492ddb4e3fa217eaf93565b653d12a2de3
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-10-15 11:03:40 +0100

    kwargs & tkinter

--- day-27/main.py
+++ day-27/main.py
@@ -1,12 +1,31 @@
-import tkinter
+from tkinter import * 
+import turtle
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 #Label
 
-my_label = tkinter.Label(text="I Am a Label!", font=("Ariel", 24, "bold"))
-my_label.pack(side="left", expand=True)
+my_label = Label(text="I Am a Label!", font=("Ariel", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text" #my_label.config(text="New Text")
+
+#Button
+
+def button_clicked():
+    #my_label["text"] = "Button Got Clicked"
+    my_label.config(text=input.get())
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#Entry
+
+input = Entry(width = 10)
+input.pack()
+print(input.get())
+
 
 window.mainloop()
--- day-27/playground.py
+++ day-27/playground.py
@@ -7,6 +7,32 @@ def add(*args):
         sum += n
     return sum
 
-print(add(1, 2, 3, 7, 8, 9, 20, 21, 33))
+#print(add(1, 2, 3, 7, 8, 9, 20, 21, 33))
 
-#def calculate(**kwargs):
+def calculate(n, **kwargs):
+    print(kwargs)
+    #for key, value in kwargs.items():
+    #    print(key)
+    #    print(value)
+
+    #print(kwargs["add"])
+
+    n += kwargs['add']
+    n *= kwargs['multiply']
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw['make']
+        #self.model = kw['model']
+        self.model = kw.get("model") # if no model value it returns None
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+#my_car = Car(make='Nissan', model='GT-R')
+my_car = Car(make='Nissan')
+print(my_car.make)
+print(my_car.model)
