8fdfe4f8e149fcd11ada57939ce5de25cfdc99f0
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-10-15 17:04:52 +0100

    tk widgets + layouts

--- day-27/main.py
+++ day-27/main.py
@@ -1,6 +1,11 @@
 from tkinter import * 
 import turtle
 
+def button_clicked():
+    print("I got clicked")
+    #my_label["text"] = "Button Got Clicked"
+    my_label.config(text=input.get())
+
 window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
@@ -8,24 +13,23 @@ window.minsize(width=500, height=300)
 #Label
 
 my_label = Label(text="I Am a Label!", font=("Ariel", 24, "bold"))
-my_label.pack()
-
 my_label["text"] = "New Text" #my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+#my_label.place(x=0, y=0)
+#my_label.pack(side = "left")
 
 #Button
 
-def button_clicked():
-    #my_label["text"] = "Button Got Clicked"
-    my_label.config(text=input.get())
-
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+#button.pack()
 
 #Entry
 
 input = Entry(width = 10)
-input.pack()
 print(input.get())
+input.grid(column=2, row=2)
+#input.pack()
 
 
 window.mainloop()
