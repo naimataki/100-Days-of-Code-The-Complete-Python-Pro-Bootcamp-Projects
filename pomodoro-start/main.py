42e01bc096a98b3bc45b7c88456299442b0e36f1
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-10-16 17:56:09 +0100

    pomodoro timer mechanism

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -10,10 +10,19 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+#def timer_reset():
+#    print("I got clicked")
+#    canvas.config(text="00:00")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    print(count)
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,7 +32,22 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="pomodoro-start/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+canvas.grid(column=1, row=1)
+count_down(5)
+
+title_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"))
+title_label.config(fg=GREEN, bg=YELLOW, highlightthickness=0)
+title_label.grid(column=1, row=0)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+check_label = Label(text="✔️", font=(FONT_NAME, 20, "bold"))
+check_label.config(fg=GREEN, bg=YELLOW, highlightthickness=0)
+check_label.grid(column=1, row=3)
 
 window.mainloop()
