9040bfc955154d614c66eafaa1c406d73bdcc450
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-10-17 07:08:18 +0100

    pomodoro countdown

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 #def timer_reset():
@@ -15,14 +17,37 @@ LONG_BREAK_MIN = 20
 #    canvas.config(text="00:00")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
 
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN
+    long_break_sec= LONG_BREAK_MIN
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else: 
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    print(count)
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    #print(count)
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -34,19 +59,18 @@ tomato_img = PhotoImage(file="pomodoro-start/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
-count_down(5)
 
 title_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"))
 title_label.config(fg=GREEN, bg=YELLOW, highlightthickness=0)
 title_label.grid(column=1, row=0)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset")
 reset_button.grid(column=2, row=2)
 
-check_label = Label(text="✔️", font=(FONT_NAME, 20, "bold"))
+check_label = Label(font=(FONT_NAME, 20, "bold"))
 check_label.config(fg=GREEN, bg=YELLOW, highlightthickness=0)
 check_label.grid(column=1, row=3)
 
