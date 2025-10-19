a456dd9980017fc1c1cf731be7240bba06074164
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-10-19 16:41:14 +0100

    password manager ui

--- password-manager/main.py
+++ password-manager/main.py
@@ -6,7 +6,7 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="password-manager/logo.png")
@@ -16,10 +16,27 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
+website_input = Entry(width = 50)
+website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
+
 username_label = Label(text="Email/Username:")
 username_label.grid(column=0, row=2)
 
+username_input = Entry(width = 50)
+username_input.grid(column=1, row=2, columnspan=2)
+username_input.insert(0, "takioutinaima10@gmail.com")
+
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
+
+password_input = Entry(width = 32)
+password_input.grid(column=1, row=3)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=43)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
