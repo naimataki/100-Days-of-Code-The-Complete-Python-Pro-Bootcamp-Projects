3b731288e8ab2e1234b4dc1cc1563b52cf5e75dc
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-12-19 22:46:30 +0100

    Saving progress start

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -8,22 +8,38 @@ data = pandas.read_csv("flash-card-project/data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 #print(to_learn)
 #print(data)
+current_card = {}
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    print(current_card["French"])
-    canvas.itemconfig(language_text, text="French")
-    canvas.itemconfig(word_text, text=current_card["French"])
+    #print(current_card["French"])
+    canvas.itemconfig(language_text, text="French", fill="black")
+    canvas.itemconfig(word_text, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_card)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(language_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_card)
+
+def is_known():
+    to_learn.remove(current_card)
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card = PhotoImage(file="flash-card-project/images/card_front.png")
-canvas.create_image(400, 263, image=front_card)
-language_text = canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 40, "italic"))
-word_text = canvas.create_text(400, 263, text="word", fill="black", font=("Ariel", 60, "bold"))
+back_card = PhotoImage(file="flash-card-project/images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_card)
+language_text = canvas.create_text(400, 150, text="", fill="black", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="flash-card-project/images/right.png")
@@ -33,6 +49,8 @@ right_button.grid(column=1, row=1)
 wrong_image = PhotoImage(file="flash-card-project/images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
+
+next_card()
 
 window.mainloop()
 
