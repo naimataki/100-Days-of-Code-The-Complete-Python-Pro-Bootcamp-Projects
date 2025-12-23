afba59e6e557c51440401654f415f0a6b0f40887
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2025-12-23 14:11:06 +0100

    Challenge 1 - Send Motivational Quotes on Mondays via Email

--- Birthday Wisher/Birthday Wisher (Day 32) start/main.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-my_email = "naimatakiouti24@gmail.com"
-
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password="zdw f oomn wsl l zamz")
-connection.sendmail(from_addr=my_email, to_addrs="takioutinaima10@gmail.com", msg="Hello")
-connection.close()
---
+++ Birthday Wisher/main.py
@@ -0,0 +1,49 @@
+import smtplib
+#
+my_email = "naimatakiouti24@gmail.com"
+password = "zdw f oomn wsl l zamz"
+#
+#with smtplib.SMTP("smtp.gmail.com") as connection:
+#    connection.starttls()
+#    connection.login(user=my_email, password=password)
+#    connection.sendmail(
+#        from_addr=my_email, 
+#        to_addrs="takioutinaima10@gmail.com", 
+#        msg="Subject:Hello\n\nThis is the body of my email.")
+##connection.close()
+
+import datetime as dt
+import random
+#
+#now = dt.datetime.now() #actual time rn
+#year = now.year
+#print(type(year))
+#print(now) #type: datetime object
+#if year == 2025:
+#    print("We host the Afcon in Morocco")
+#month = now.month
+#day_of_week = now.weekday()
+#print(day_of_week)
+#
+#birthday = dt.datetime(year=2001, month=3, day=23, hour=6)
+#print(birthday)
+
+now = dt.datetime.now()
+
+day_of_week = now.weekday()
+
+with open("Birthday Wisher/quotes.txt") as file:
+    quotes = file.readlines()
+    quote = random.choice(quotes)
+    print(quote.strip())
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email, 
+            to_addrs="takioutinaima10@gmail.com", 
+            msg=f"Subject:Hello\n\n{quote.strip()}")
+
+
+
+
