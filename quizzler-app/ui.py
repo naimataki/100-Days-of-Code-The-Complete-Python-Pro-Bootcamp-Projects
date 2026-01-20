db97647100fdc2b5709b294532ae9d5abff190b1
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-01-20 10:35:32 +0100

    Using API Keys to Authenticate and Get the Weather from OpenWeatherMap

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -11,8 +11,8 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.title_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
-        self.title_label.grid(column=1, row=0)
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 
@@ -23,11 +23,11 @@ class QuizInterface:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         right_image = PhotoImage(file="quizzler-app/images/true.png")
-        self.right_button = Button(image=right_image, highlightthickness=0)
+        self.right_button = Button(image=right_image, highlightthickness=0, command=self.true_pressed)
         self.right_button.grid(column=0, row=2)
 
         wrong_image = PhotoImage(file="quizzler-app/images/false.png")
-        self.wrong_button = Button(image=wrong_image, highlightthickness=0)
+        self.wrong_button = Button(image=wrong_image, highlightthickness=0, command=self.false_pressed)
         self.wrong_button.grid(column=1, row=2)
 
         self.get_next_qustion()
@@ -35,5 +35,25 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_qustion(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions:
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.config(self.question_text, text="You've reached the end of the quiz.")
+            self.right_button.config(state="disabeled")
+            self.false_button.config(state="disabeled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_qustion)
