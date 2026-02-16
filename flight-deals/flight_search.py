073efea98254d40624e652f949b8d7f211265b5f
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-02-16 07:49:07 +0000

    Search for Cheap Flights

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 API_KEY = os.environ["AMADEUS_API_KEY"]
 API_SECRET = os.environ["AMADEUS_API_SECRET"]
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
@@ -13,6 +14,24 @@ class FlightSearch:
         self._api_key = API_KEY
         self._api_secret = API_SECRET
         self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': API_KEY,
+            'client_secret': API_SECRET
+        }
+        response = requests.post(
+                                url="https://test.api.amadeus.com/v1/security/oauth2/token", 
+                                headers=header, 
+                                data=body)
+        data = response.json()
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return data["access_token"]
 
     def get_destination_code(self, city_name):
         print(f"Using this token to get destination {self._token}")
@@ -38,20 +57,32 @@ class FlightSearch:
         #code = "TESTING"
         return code
     
-    def _get_new_token(self):
-        header = {
-            'Content-Type': 'application/x-www-form-urlencoded'
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "max": "10"
         }
-        body = {
-            'grant_type': 'client_credentials',
-            'client_id': API_KEY,
-            'client_secret': API_SECRET
-        }
-        response = requests.post(
-                                url="https://test.api.amadeus.com/v1/security/oauth2/token", 
-                                headers=header, 
-                                data=body)
-        data = response.json()
-        print(f"Your token is {response.json()['access_token']}")
-        print(f"Your token expires in {response.json()['expires_in']} seconds")
-        return data["access_token"]
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n" \
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+        
+        return response.json()
+
