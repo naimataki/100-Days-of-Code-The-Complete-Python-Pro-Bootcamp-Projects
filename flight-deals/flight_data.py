e4fcdd9894f4c1fb73d05548f2873dae8cf8de6a
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-02-17 20:12:22 +0000

    Notification manager

--- flight-deals/flight_data.py
+++ flight-deals/flight_data.py
@@ -7,30 +7,30 @@ class FlightData:
         self.out_date = out_date
         self.return_date = return_date
     
-    def find_cheapest_flight(data):
-        if data is None or not data['data']:
-            print("No flight data")
-            return FlightData("N/A", "N/A", "N/A", "N/A", "N/A")
+def find_cheapest_flight(data):
+    if data is None or not data['data']:
+        print("No flight data")
+        return FlightData("N/A", "N/A", "N/A", "N/A", "N/A")
 
-        first_flight =  data["data"][0]
-        lowest_price = float(first_flight["price"]["grandTotal"])
-        origin = first_flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
-        destination = first_flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
-        out_date = first_flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
-        return_date = first_flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
+    first_flight =  data["data"][0]
+    lowest_price = float(first_flight["price"]["grandTotal"])
+    origin = first_flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+    destination = first_flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
+    out_date = first_flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
+    return_date = first_flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
 
-        cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+    cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
 
-        for flight in data["data"]:
-            price = float(flight["price"]["grandTotal"])
-            if price < lowest_price:
-                lowest_price = price
-                origin = flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
-                destination = flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
-                out_date = flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
-                return_date = flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
-                cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
-                print(f"Lowest price to {destination} is £{lowest_price}")
+    for flight in data["data"]:
+        price = float(flight["price"]["grandTotal"])
+        if price < lowest_price:
+            lowest_price = price
+            origin = flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+            destination = flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
+            out_date = flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
+            return_date = flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
+            cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+            print(f"Lowest price to {destination} is £{lowest_price}")
 
 
-        return cheapest_flight
+    return cheapest_flight
--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,14 +1,18 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 import time
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 print(sheet_data)
 
+ORIGIN_CITY_IATA = "LON"
+
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
         time.sleep(2)
@@ -16,3 +20,18 @@ if sheet_data[0]["iataCode"] == "":
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    time.sleep(2)
