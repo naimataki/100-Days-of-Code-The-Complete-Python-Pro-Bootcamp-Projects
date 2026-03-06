48d78f6a1387662ad0108520e6ddd0c51199f408
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-03-06 08:46:32 +0000

    Scraping Hacker News

--- bs4/main.py
+++ bs4/main.py
@@ -4,6 +4,41 @@ import requests
 response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
 
+soup = BeautifulSoup(yc_web_page, "html.parser")
+#print(soup.title)
+
+articles = soup.find_all(name="span" ,class_="titleline")
+article_texts = []
+article_links = []
+for title in articles:
+    text = title.find(name="a").getText()
+    article_texts.append(text)
+    link = title.find(name="a").get("href")
+    article_links.append(link)
+    #print(headline.getText())
+
+title = soup.find(name="span", class_="titleline")
+article_tag = title.find(name="a")
+article_text = article_tag.getText()
+article_link = article_tag.get("href")
+article_upvotes = [upvote.getText() for upvote in soup.find_all(name="span", class_="score")]
+int_upvotes = [int(upvote.split()[0]) for upvote in article_upvotes]
+
+index_of_heighest_upvotes = int_upvotes.index(max(int_upvotes))
+print(article_texts[index_of_heighest_upvotes])
+print(article_links[index_of_heighest_upvotes])
+
+
+
+#print(article_texts)
+#print(article_links)
+#print(article_upvotes)
+
+#print(int_upvotes)
+
+
+
+
 #with open("bs4/website.html") as file:
 #    contents = file.read()
 #
