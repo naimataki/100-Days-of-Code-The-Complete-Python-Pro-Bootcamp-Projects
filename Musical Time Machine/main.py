a317bf18851a7b77a101e3c431ecf7c824e16950
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-03-07 09:04:59 +0000

    Authentication with Spotify

--- Musical Time Machine/main.py
+++ Musical Time Machine/main.py
@@ -1,12 +1,42 @@
 from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+load_dotenv()
+
+CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
+CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
+REDIRECT_URI = os.environ["SPOTIPY_REDIRECT_URI"]
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope="playlist-modify-private"
+    )
+)
+
+results = sp.current_user_saved_tracks()
+
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], "-", track['name'])
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"}
 
-#response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
-#empire_web_page = response.text
-#
-#soup = BeautifulSoup(empire_web_page, "html.parser")
+response = requests.get(f"https://www.billboard.com/charts/hot-100/{date}", headers=header)
+billboard_web_page = response.text
+#print(billboard_web_page)
 
+soup = BeautifulSoup(billboard_web_page, "html.parser")
+
+songs = soup.select("li ul li h3")
+song_titles = [song.getText().strip() for song in songs]
+
+print(song_titles)
