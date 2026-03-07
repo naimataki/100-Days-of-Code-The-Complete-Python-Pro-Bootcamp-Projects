807fba28a10415ec81dc9f8c86870a8cdb75f7ad
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-03-07 06:36:41 +0000

    Scraping the Billboard Hot 100

---
+++ Musical Time Machine/main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"}
+
+#response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+#empire_web_page = response.text
+#
+#soup = BeautifulSoup(empire_web_page, "html.parser")
+
