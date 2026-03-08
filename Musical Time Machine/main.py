e133bc47d1d3026dc0c3e572a567bccb40bebbe4
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-03-08 08:21:02 +0000

    Creating and Adding to Spotify Playlist

--- Musical Time Machine/main.py
+++ Musical Time Machine/main.py
@@ -20,11 +20,7 @@ sp = spotipy.Spotify(
     )
 )
 
-results = sp.current_user_saved_tracks()
-
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], "-", track['name'])
+user_id = sp.current_user()["id"]
 
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
@@ -38,5 +34,21 @@ soup = BeautifulSoup(billboard_web_page, "html.parser")
 
 songs = soup.select("li ul li h3")
 song_titles = [song.getText().strip() for song in songs]
-
 print(song_titles)
+song_uris = []
+for song in song_titles:
+    result = sp.search(q=f"track: {song} year: {date.split("-")[0]}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in spotify. skipped.")
+
+playlist = sp.user_playlist_create(
+    user=user_id,
+    name=f"{date} Billboard 100",
+    public=False
+)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
